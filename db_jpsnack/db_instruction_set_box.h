0bf9bd5a8a09e7dfbac8880b489e5f032cdf1967
faiz_sejong <faiz@sju.ac.kr>
2023-12-22 16:25:19 +0900

    updated2

--- db_jpsnack/db_instruction_set_box.h
+++ db_jpsnack/db_instruction_set_box.h
@@ -14,10 +14,15 @@ namespace dbench {
 
 	private:
 		uint16_t ityp_{ 0 };
+		bool instructions_present_{ false };
+		bool xo_yo_present_{ false };
+		bool width_height_present_{ false };
+		bool life_nextuse_persis_present_{ false };
+		bool crop_params_present_{ false };
+		bool rotation_present_{ false };
 		uint16_t rept_{ 0 };
 		uint32_t tick_{ 0 };
 		vector<DbJPSnackInstruction> instructions_;
-
 
 
 		
